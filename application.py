73d7e79ec3cdba3689b24258c505048398b482d5
윤상현 <ysh@zigbang.com>
2022-07-04 18:25:54 +0900

    init. project

---
+++ application.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import inferencer as inf
+import json
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/inference', methods=['POST'])
+def inference_uploaded_file():
+    f = request.files['img']
+
+    return {"result": inf.run_inference(f)}
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ inferencer.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+import os
+import onnxruntime as ort
+import numpy as np
+
+def run_inference(file):
+    print(file)
+
+    # read file
+    npimg = np.fromstring(file.read(), np.uint8)
+    image = cv2.imdecode(npimg,1)
+
+    # convert, resize
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.resize(gray, (28,28)).astype(np.float32)/255
+    
+    # reshape
+    ipt = np.reshape(gray, (1,1,28,28))
+
+    # init. onnx runtime
+    ort_sess = ort.InferenceSession('mnist-7.onnx')
+
+    # for  in ort_sess.get_inputs():
+    arg = ort_sess.get_inputs()[0]
+    outputs = ort_sess.run(None, {'Input3': ipt})
+    
+    # Print Result
+    result = outputs[0].argmax(axis=1)
+
+    return int(result[0])
+
