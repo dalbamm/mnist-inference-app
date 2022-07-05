2bc7437a207b75058a6c7138afb398ac58772d03
윤상현 <ysh@zigbang.com>
2022-07-05 16:47:43 +0900

    feat: update logics

--- application.py
+++ application.py
@@ -5,15 +5,46 @@ import json
 
 app = Flask(__name__)
 
+inferencer = inf.Inferencer()
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 @app.route('/inference', methods=['POST'])
 def inference_uploaded_file():
-    f = request.files['img']
+    f = request.files['img']    
+    return {"result": inferencer.run_inference(f)}
 
-    return {"result": inf.run_inference(f)}
+@app.route('/inference_queue', methods=['POST'])
+def inference_uploaded_file_queue():
+    f = request.files['img']
+    return {"result": inferencer.run_inference_queue(f)}
+
+
+@app.route('/inference_multi_s', methods=['POST'])
+def inference_uploaded_multi_files_serial():
+    f = request.files.getlist('img')
+    # print(f)
+    return {"result": inferencer.run_inference_multiple_serial(f)}
+
+@app.route('/inference_multi_s_t', methods=['POST'])
+def inference_uploaded_multi_files_serial_threading():
+    f = request.files.getlist('img')
+    return {"result": inferencer.run_inference_multiple_threading_wo_option(f)}
+
+
+@app.route('/inference_multi_p', methods=['POST'])
+def inference_uploaded_multi_files_parallel():
+    f = request.files.getlist('img')
+    # print(f)
+    return {"result": inferencer.run_inference_multiple_parallel(f)}
+
+@app.route('/inference_multi_p_t', methods=['POST'])
+def inference_uploaded_multi_files_parallel_threading():
+    f = request.files.getlist('img')
+    # print(f)
+    return {"result": inferencer.run_inference_multiple_threading_w_option(f)}
 
 if __name__ == '__main__':
     app.run(debug=True)
